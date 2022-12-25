6df2fc2d10b4090fce6536ad407702a3395ec5ec
SaadAyouj <saadayoujweb@gmail.com>
2022-12-26 00:29:53 +0100

    feat: add Nav and Footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,18 @@
 import type { AppProps } from 'next/app';
+import { ReactElement } from 'react';
+import Footer from '../components/common/footer/Footer';
+import Nav from '../components/common/nav/Nav';
 import '../styles/globals.css';
+
+function Layout({ children }: { children: ReactElement }) {
+  return (
+    <>
+      <Nav />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
